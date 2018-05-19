0e395e8ef5b64f97794c0495a115b40fcb9729b0
wakary <redoume@163.com>
2018-05-19 21:23:37 +0800

    update

---
+++ MYPEmotionKeyboard/Classes/MYPEmotion.swift
@@ -0,0 +1,18 @@
+//
+//  MYPEmotion.swift
+//  MYPEmotionKeyboard
+//
+//  Created by wakary redou on 2018/5/19.
+//
+
+import Foundation
+
+public class MYPEmotion {
+    public var name: String
+    public var description: String
+    
+    init(name: String, description: String) {
+        self.name = name
+        self.description = description
+    }
+}
