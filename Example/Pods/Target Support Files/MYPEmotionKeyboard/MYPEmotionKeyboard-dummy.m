0c3e233e7520fce95addade9507c448bef1aafd2
wakary <redoume@163.com>
2018-05-19 16:34:30 +0800

    init
    new pod

---
+++ Example/Pods/Target Support Files/MYPEmotionKeyboard/MYPEmotionKeyboard-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MYPEmotionKeyboard : NSObject
+@end
+@implementation PodsDummy_MYPEmotionKeyboard
+@end
---
+++ Example/Pods/Target Support Files/Pods-MYPEmotionKeyboard_Tests/Pods-MYPEmotionKeyboard_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MYPEmotionKeyboard_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_MYPEmotionKeyboard_Tests
+@end
