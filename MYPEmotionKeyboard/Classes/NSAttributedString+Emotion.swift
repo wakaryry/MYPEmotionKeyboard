b2dfa697bb9cf2f372243250142e5e1c6cbcfc88
wakary <redoume@163.com>
2018-05-21 23:50:03 +0800

    update

--- MYPEmotionKeyboard/Classes/NSAttributedString+Emotion.swift
+++ MYPEmotionKeyboard/Classes/NSAttributedString+Emotion.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+extension NSAttributedString {
+    /** 匹配给定attributedString中的所有emoji，如果匹配到的emoji有本地图片的话会直接换成本地的图片*/
+    public func translateAttributedTextIntoEmotionText(with font: UIFont?) {
+        if self.length == 0 || font == nil {
+            return
+        }
+    }
+}
